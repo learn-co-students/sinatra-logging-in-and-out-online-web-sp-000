65e0ac2c7a60f0738b477e14b4eb70d66c99ab27
Douglas Uretsky <douglasuretsky@gmail.com>
2018-12-06 23:35:57 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,9 @@
 class Helpers
+  def self.current_user(sess_hash)
+    @current = User.find(sess_hash[:user_id])
+  end
+  
+  def self.is_logged_in?(sess_hash)
+    @current.id == sess_hash[:user_id]
+  end
 end
