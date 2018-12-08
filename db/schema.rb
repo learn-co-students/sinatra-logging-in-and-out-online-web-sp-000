c94de874a3b5ae2284196b7010f022479505142e
aburk3 <aburk3@gmail.com>
2018-12-08 15:42:42 -0600

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181208195822) do
+ActiveRecord::Schema.define(version: 20181208213823) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
-    t.string "balance"
+    t.decimal "balance"
   end
 
 end
