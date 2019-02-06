381df0ca9b3d32f48a235ab8a60b3155601b4664
Nicole D <nnardella@gmail.com>
2019-02-06 18:37:33 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,8 +21,11 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    if Helpers.is_logged_in?(session)
-       @user = Helpers.current_user(session)
+    # if Helpers.is_logged_in?(session)
+    #    @user = Helpers.current_user(session)
+      if @user = User.find_by(id: session[:user_id])
+
+
        erb :account
     else
      erb :error
