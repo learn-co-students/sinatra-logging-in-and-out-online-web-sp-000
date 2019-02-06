15530ce9aec32db58820ea78014781f25f9a61ac
Nicole D <nnardella@gmail.com>
2019-02-06 17:13:18 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,6 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(username: params[:username], password: params[:password])
-    binding.pry
     if @user
       session[:user_id] = @user.id
       redirect '/account'
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,11 @@
 class Helpers
+
+  def current_user(session)
+
+  end
+
+
+  def is_logged_in?(session)
+  end
+  
 end
