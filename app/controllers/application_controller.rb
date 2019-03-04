ccbdccc6a2de2d1537b4ee9d9f5fd21c2ff13604
jonmackeyparty <jon@jonmackey.party>
2019-03-04 14:37:11 -0500

    complete the many tasks of this lesson

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,26 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params[:username])
+    if @user
+      session[:user_id] = @user.id
+      redirect to '/account'
+    end
+      erb :error
   end
 
   get '/account' do
-
+      erb :account
   end
 
   get '/logout' do
+    session.clear
+    redirect to '/'
+  end
 
+  get '/error' do
+    erb :error
   end
 
 
 end
-
