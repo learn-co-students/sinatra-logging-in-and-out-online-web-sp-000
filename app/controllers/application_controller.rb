c9e16274225d6e38aeab9592bde14ef8934fe2f1
Robert Tate <btate712@gmail.com>
2019-04-24 01:24:01 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,11 +21,11 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    @user = User.find(session[:user_id]
     if Helpers.is_logged_in?(session)
+      @user = User.find(session[:user_id])
       erb :account
     else
-      redirect '/error'
+      erb :error
     end
   end
 
