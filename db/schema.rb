a43da662de43f1c0fa07042b690ec4044bb5fa57
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-26 05:14:27 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,9 @@
 ActiveRecord::Schema.define(version: 20190426040620) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+    t.string "username"
     t.string "password"
+    t.integer "balance"
   end
 
 end
