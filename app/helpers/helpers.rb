82de63201ac8bab2bdbe9d5bdf6548449ba88acc
Mackenzie Moore <macmoore@ucdavis.edu>
2019-04-26 00:20:16 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,13 +1,9 @@
 class Helpers
   def self.current_user(session)
-    found = User.find(session[:user_id])
+    @user = User.find(session[:user_id])
   end
 
   def self.is_logged_in?(session)
-    if self.current_user(session) != nil
-      true
-    else
-      false
-    end
+    !!session[:user_id]
   end
 end
