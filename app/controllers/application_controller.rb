0a35696e26c1543f51135f69de937057f98d9c9c
Tommy Curley <thomas.curley93@gmail.com>
2019-05-13 22:36:22 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,15 +11,25 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    if User.exists?(username: params[:username], password: params[:password])
+      session[:user_id] = User.find_by(username: params[:username], password: params[:password]).id
+      redirect to '/account'
+    end
+    erb :error
   end
 
   get '/account' do
-
+    if session[:user_id].nil?
+      erb :error
+    else
+      @user = User.find(session[:user_id])
+      erb :account
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect to '/'
   end
 
 
--- config.ru
+++ config.ru
@@ -1,6 +1,6 @@
 require './config/environment'
 
-if ActiveRecord::Base.connection.migration_context.needs_migration?
+if defined?(ActiveRecord::Migrator) && ActiveRecord::Migrator.needs_migration?
   raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
 end
 
