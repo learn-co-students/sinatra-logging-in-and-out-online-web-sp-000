9aa60ea3cc6814bab94923b10abeb2c0fcb39e33
Alan Krajina <alankrajina88@gmail.com>
2019-07-18 16:41:15 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -7,11 +7,7 @@ class Helpers
   
   
   def self.is_logged_in?(session)
-    if @user.id == session[:user_id]
-     return true
-    else
-      return false
-    end
+    !!session[:user_id]
   end
   
   
