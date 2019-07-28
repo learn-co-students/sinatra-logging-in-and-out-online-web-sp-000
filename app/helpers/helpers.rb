1f8a4fb8e6fca452bcd7c4debe52b9a9c6964b1c
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-07-28 23:52:37 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,9 +1,9 @@
 class Helpers
-  def current_user(session)
+  def self.current_user(session)
     User.find(session[:user_id])
   end
 
-  def is_logged_in?(session)
+  def self.is_logged_in?(session)
     !!session[:user_id]
   end
 end
