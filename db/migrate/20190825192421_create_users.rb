f0c2b5a8593d9fcac08e6e9c0a920338b26baca8
Meisuleen Kased <meisuleen@gmail.com>
2019-08-25 20:07:46 +0000

    Done.

--- db/migrate/20190825192421_create_users.rb
+++
@@ -1,4 +0,0 @@
-class CreateUsers < ActiveRecord::Migration[5.1]
-  def change
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190825192421) do
+ActiveRecord::Schema.define(version: 20190825193840) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.decimal "balance"
+    t.string "username"
     t.string "password"
+    t.decimal "balance"
   end
 
 end
