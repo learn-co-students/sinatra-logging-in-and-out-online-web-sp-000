08e2f9ed3f315e167d18a179ca01a6841d105c5e
Muhammad Musah <muhammad@Muhammads-Machine.local>
2019-08-31 16:27:34 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(:username => params[:username])
-    # binding.pry
+    binding.pry
     # if @user != nil && @user.password == params[:password]
     if @user
       session[:user_id] = @user.id
