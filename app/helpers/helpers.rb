134c0dc1fe29a2465298db95210f5f569f179467
Rehman Ali <rehmanali001@gmail.com>
2019-09-18 20:29:50 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,12 @@
 class Helpers
-end
+
+  def self.current_user(session) 
+    # binding.pry
+    @user = User.find_by_id(session[:user_id])
+  end
+
+  def self.is_logged_in?(session)
+    # binding.pry
+    !!session[:user_id]
+  end
+end 	
