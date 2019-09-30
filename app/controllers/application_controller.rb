5d84c4a3ff29a81d9653eefed7308c07aa14bd33
Alice Brunel <alc.brunel@gmail.com>
2019-09-30 10:22:16 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,15 +11,21 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(:username => params[:username])
+    
+    if @user.username
+      redirect_to '/account'
+    else
+      erb :error
+    end
   end
 
   get '/account' do
-
+    erb :acount
   end
 
   get '/logout' do
-
+    @session.clear
   end
 
 
