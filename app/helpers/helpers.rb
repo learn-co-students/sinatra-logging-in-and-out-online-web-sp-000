b09464d400c370171ff6c2e8073e213bdb6930a5
Mark Daniel  Delgado <markpnoi26@gmail.com>
2019-10-19 22:21:29 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,15 @@
+require 'sinatra/base'
+
 class Helpers
+  
+  def self.current_user(session_hash)
+    @user = User.find(session_hash[:user_id])
+    @user
+  end
+  
+  def self.is_logged_in?(session_hash)
+    # self.current_user(session_hash).id == session_hash[:user_id]
+    !!session_hash[:user_id]
+  end
+  
 end
