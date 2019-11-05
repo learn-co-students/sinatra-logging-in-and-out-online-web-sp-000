019db8822ce8d78f2a0c3d08b5e3950a4d862088
Leslie Osorio <lesosorio17@gmail.com>
2019-11-05 19:04:25 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,13 +1,12 @@
 class Helpers
 
-  def current_user(session)
-    session[:user_id] = @user_id
+  def self.current_user(session_hash)
+    @user = User.find(session_hash[:user_id])
   end
 
 
-  def is_logged_in?
-
-
+  def self.is_logged_in?(session_hash)
+    !!session_hash[:user_id]
   end
 
 end
