9d22d1d84bc125a07a2bb7257c58e31fbda46491
github username <github email address>
2019-12-01 18:44:56 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require_relative '../../config/environment'
+require 'pry'
 class ApplicationController < Sinatra::Base
   
   configure do
@@ -12,15 +13,36 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.create()
+ 
+
+    @user = User.find_by(username: params[:username],password: params[:password])
+
+    if @user # if user is found
+      session[:user_id] = @user.id
+      # session[:user_id] = @user.id
+      redirect to '/account'
+    else
+      erb :error
+    end
+
   end
 
   get '/account' do
+    # if user is logged in then 
 
+    @user = User.find_by_id(session[:user_id])
+
+    if @user #if user found => truth
+      erb :account
+    else
+      erb :error
+    end  
   end
 
   get '/logout' do
+    session.clear
 
+    redirect to '/'
   end
 
 
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,12 +1,13 @@
 class Helpers
 
-    def current_user 
-
+    # need to have class methods because we will never have instance of Helper to call on
+    def self.current_user(session) # find user by session id
+        @current_user = User.find(session[:user_id])
+        @current_user
     end
 
-    def is_logged_in?
-
+    def self.is_logged_in?(session) #check if there is a session
+        !!session[:user_id] # if the session[:user_id] is there
     end
-
 
 end
