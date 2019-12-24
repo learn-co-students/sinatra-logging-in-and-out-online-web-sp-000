e1a2bc690de810c1f64549d25c8105a922ed065f
BlythePollard <github email address>
2019-12-24 15:45:38 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,27 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params[:username], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      redirect to '/account'
+    end
+    erb :error
+    #find user in database by name
+    #if match, set session id to user id, redirect to /accoount
+    #if no match, error page
   end
 
   get '/account' do
-
+    @user = User.find_by_id(session[:user_id])
+    #can't use find_by params here because it's new page, no params have been defined here
+    erb :account
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
-
 
 end
 
