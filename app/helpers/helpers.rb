7269a0c18765fead9dbe680600be8c7f43397d26
LIManuel <leonmalisov@gmail.com>
2020-04-03 11:17:43 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,16 @@
 class Helpers
+	attr_accessor :id
+
+	def self.current_user(session_hash)
+		User.find_by(id: session_hash[:user_id])
+	end
+
+	def self.is_logged_in?(session_hash)
+		!!session_hash[:user_id]
+		# if session_hash.keys.include?(:user_id)
+		# 	true 
+		# else
+		# 	false
+		# end
+	end
 end
