e694dd1a9815058047e19f50d151c434402eb0ec
ksummerill <kaitlynsummerill@gmail.com>
2020-04-20 09:28:49 -0700

    Sinatra logging in lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,29 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params["username"])
+    if @user
+      # set session to user's ID and redirect to /account
+      session[:user_id] = @user.id
+      redirect '/account'
+    else
+      erb :error
+    end
   end
 
   get '/account' do
-
+    if !session[:user_id]
+      erb :error
+    else
+      @user = User.find(session[:user_id])
+      erb :account
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
 
 
 end
-
