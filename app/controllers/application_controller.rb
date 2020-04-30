28adac2c27b9f761de2384cde853820d31baa94f
jkellyphilly <joel.kelly.philly@gmail.com>
2020-04-30 11:19:30 -0400

    finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,9 +23,13 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    @user = User.find(session[:user_id])
+    if Helpers.is_logged_in?(session)
+      @user = Helpers.current_user(session)
 
-    erb :account
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
