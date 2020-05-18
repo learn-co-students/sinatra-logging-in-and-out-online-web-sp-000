7c1ba0ae75bce564cdb1314733fd551b20d6f738
bruffin1221 <bruffin1221@gmail.com>
2020-05-18 15:33:48 -0700

    Done.

---
+++ db/migrate/20200516215551_createusertable.rb
@@ -0,0 +1,11 @@
+class Createusertable < ActiveRecord::Migration[5.1]
+  
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.decimal :balance
+    end
+  end
+
+end
