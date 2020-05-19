c4049923fe48cdccbfebdd84b7a6b9370bc12959
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2020-05-18 21:11:50 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,13 +22,16 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    erb :account
+    if Helpers.is_logged_in(session)
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
-
-
 end
 
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,5 +1,10 @@
 class Helpers
-    def current_user 
-        @user = User.all.find{|u| u.username = params[:username]}
+    def self.current_user(session)
+        @user = User.find_by_id(session[:user_id])
+
+    end
+
+    def self.is_logged_in?(session)
+        !!session[:user_id]
     end
 end
