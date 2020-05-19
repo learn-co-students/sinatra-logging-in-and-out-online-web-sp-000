767dd8510ace883ab27c2167ebf24e2fba2d469d
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2020-05-18 21:32:23 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    if Helpers.is_logged_in(session)
+    if Helpers.is_logged_in?(session)
       erb :account
     else
       erb :error
