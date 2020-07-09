237329c92129f6a5e3de636c72be288cb18df3b6
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-07-09 00:00:17 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,11 +23,16 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-
+    if Helpers.is_logged_in?(session)
+      erb :account
+    else 
+     erb :error 
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect to '/'
   end
 
 
