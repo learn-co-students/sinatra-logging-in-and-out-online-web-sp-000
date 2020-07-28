765d440b2dc61e06d82e6805bdd55f094805f20b
concow <ccowan003@comcast.net>
2020-07-28 16:17:43 -0700

    Done.

--- db/migrate/01_create_users_table.rb
+++ db/migrate/01_create_users_table.rb
@@ -3,7 +3,7 @@ class CreateUsersTable < ActiveRecord::Migration[5.1] #Don't forget to specify w
         create_table :users do |t|
             t.string :username 
             t.string :password
-            t.string :balance
+            t.decimal :balance              #decimal here was important!
         end
     end
 end
