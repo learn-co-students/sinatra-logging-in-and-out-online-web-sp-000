8ae98b8167413b2125f4302820403751d3a90ae8
concow <ccowan003@comcast.net>
2020-07-28 16:12:09 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
+require_relative '../../config/environment'
 class ApplicationController < Sinatra::Base
-  register Sinatra::ActiveRecordExtension
   configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
+    set :views, Proc.new { File.join(root, "../views/") }
     enable :sessions unless test?
     set :session_secret, "secret"
   end
