bec3c5db310f40baae1394ae2109f8e87b9ae407
concow <ccowan003@comcast.net>
2020-07-28 14:39:14 -0700

    Done.

--- db/migrate/01_create_users_table.rb
+++ db/migrate/01_create_users_table.rb
@@ -1,7 +1,7 @@
-class CreateUsersTable < ActiveRecord::Migration
+class CreateUsersTable < ActiveRecord::Migration[5.1] #Don't forget to specify which version
     def change
         create_table :users do |t|
-            t.string :username
+            t.string :username 
             t.string :password
             t.string :balance
         end
