a00bb78ac02e1a9977209705871e14cd33c6b003
Umnums <josephtkao@gmail.com>
2020-08-13 18:57:32 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    
 
   end
 
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,11 @@
-class Helpers
+class Helpers < ActiveRecord::Base
+    def self.current_user(session)
+        user = User.find(session[:user_id])
+    end
+
+    def self.is_logged_in?(session)
+        if User.find(session[:user_id])
+        end
+    end
+
 end
