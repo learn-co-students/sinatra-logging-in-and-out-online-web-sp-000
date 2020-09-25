bbe347b964ad65de279fd546ae906e9d74ae2f7d
reevesalic <reevesalic@gmail.com>
2020-09-25 13:48:11 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,14 +20,19 @@ else
 end
 end
 
-  get '/account' do
-
+get '/account' do
+  @current_user = User.find_by_id(session[:user_id])
+  if @current_user
+    erb :account
+  else
+    erb :error
   end
+end
 
-  get '/logout' do
-
-  end
-
+get '/logout' do
+  session.clear
+  redirect to '/'
+end
 
 end
 
