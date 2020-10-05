f1ccfc925ec3798278ea7733c63a5ba3a42e258a
AmyFong82 <amyhfong@gmail.com>
2020-10-04 23:46:00 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,8 +14,8 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     user = User.find_by(username: params[:username], password: params[:password])
-              binding.pry
     if user.instance_of? User
+      session[:user_id] = user[:id]
       redirect to '/account'
     else
       erb :error
@@ -24,6 +24,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
+    # @user = User.find_by(username: params[:username])
     erb :account
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-	attr_accessor :username, :password, :balance
 	
 end
 
