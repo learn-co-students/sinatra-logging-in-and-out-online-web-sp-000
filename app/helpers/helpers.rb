2b3e70b33c005fa585a3ce6195fefd8a6bfb7f09
kkirby16 <kkirby16@my.whitworth.edu>
2020-10-08 18:28:06 -0700

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -3,17 +3,9 @@ require "pry"
 class Helpers
   def self.current_user(session)
     @user = User.find(session[:user_id])
-    if @user
-      return @user
-    end
   end
 
   def self.is_logged_in?(session)
-    @user = User.find(session[:user_id])
-    if @user
-      return true
-    else
-      return false
-    end
+    !!session[:user_id]
   end
 end
