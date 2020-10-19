4b973d2756e962cc64993f60d0071c165d01ab0e
jrog20 <jaimerogstad@gmail.com>
2020-10-19 19:17:20 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,8 +27,7 @@ class ApplicationController < Sinatra::Base
   get '/account' do
     # displays the account information if a user is logged in
     @user = User.find_by(username: params[:username], password: params[:password])
-    # binding.pry
-    if @user
+    if session[:user_id] = @user.id
       redirect '/account'
     else
       # shows the error page if user goes directly to /account
