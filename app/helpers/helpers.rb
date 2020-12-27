ac40937efd6112cfa3b5ba73f6f31f2b40ab0691
Roshanak Agahi <rosh.agahi@gmail.com>
2020-12-27 03:45:24 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
 class Helpers
+  def self.current_user(session_hash)
+    @user = User.find(session_hash[:user_id])
+    return @user
+  end
+  def self.is_logged_in?(session_hash)
+    if session_hash[:user_id]
+      return true
+    else
+      return false
+    end
+  end
 end
