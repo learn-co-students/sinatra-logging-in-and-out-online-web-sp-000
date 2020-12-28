18c8098994a22521172e6196511eef0e836509aa
Michael Fuget <michaelfuget@gmail.com>
2020-12-28 04:13:54 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,19 +7,46 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+
+    @session = session
+
     erb :index
   end
 
   post '/login' do
+    is_valid_username = false
+    User.all.each do |user|
+      if user.username == params[:username]
+        is_valid_username = true 
+      end 
+    end
+    if is_valid_username
+      user = User.find_by({:username => params[:username]})
+      session[:user_id] = user.id
+      redirect '/account'
+
+    else
+      session[:user_id] = nil
+      erb :error
+    end
 
   end
 
   get '/account' do
+    @session = session
 
+    if (Helpers.is_logged_in?(session))
+      erb :account
+    else 
+      erb :error
+    end
   end
 
   get '/logout' do
+    session[:user_id] = nil
+    @session = session
 
+    redirect '/'
   end
 
 
