826bc103cf5b7f6fa59d9d9cf1118e80e3e4c1b1
bongofury <jtroutmanman@gmail.com>
2020-12-30 14:39:52 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,15 +11,28 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params[:username])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/account'
+    else
+      'You Must <a href="/">Log In</a> to View Your Balance'
+    end
   end
 
   get '/account' do
+    if Helpers.is_logged_in?(session)
+      @user = Helpers.current_user(session)
 
+      erb :account
+    else
+      'You Must <a href="/">Log In</a> to View Your Balance'
+    end
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
 
 
