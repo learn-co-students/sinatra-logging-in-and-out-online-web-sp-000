35c6ac82d1bd75378a58d6b442182de439110f79
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-02-17 12:59:12 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,11 +14,10 @@ class ApplicationController < Sinatra::Base
   post '/login' do
     #binding.pry
     @user = User.find_by(:username => params[:username])
-    if params[:password] = @user.password && @user != nil
-      #binding.pry
+    if params[:password] == @user.password && @user != nil
+      binding.pry
       session[:user_id] = @user.id
     redirect '/account' 
-    erb :account
     else
     erb :error
     end
