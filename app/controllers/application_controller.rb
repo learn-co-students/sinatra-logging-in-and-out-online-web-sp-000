349167d4a0ae9ed158b6459f886e4e2c5ac57335
Shayan Mohsin <shaymotionfilms@gmail.com>
2021-03-04 15:38:14 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,4 +35,3 @@ class ApplicationController < Sinatra::Base
 
 
 end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20210304153530) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.float "balance"
+  end
 
 end
