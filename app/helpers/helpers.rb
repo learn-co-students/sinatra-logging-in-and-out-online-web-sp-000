e9f1ba21f501417648682fe15b6e05cc474ba8e7
remote-torrent-6747 <abbyc@sky.com>
2021-04-18 13:59:29 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,12 @@
 class Helpers
+
+  def self.current_user(session)
+    id = session[:user_id]
+    User.find_by(id: id)
+  end
+
+  def self.is_logged_in?(session)
+    !!session[:user_id]
+  end
+
 end
