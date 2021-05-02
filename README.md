ad9ce02fc6b7f33a7dd2a422feaf2af9e263d26d
imperative-scientist-4600 <nelom3@gmail.com>
2021-05-02 14:17:46 +0000

    Done.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # Sinatra Sessions Lab - User Logins
 
 ## Introduction
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,10 @@
 class Helpers
+  
+  def self.current_user(s)
+    User.find_by(id: s[:user_id])
+  end
+  
+  def self.is_logged_in?(s)
+    !!User.find_by(id: s[:user_id])
+  end
 end
