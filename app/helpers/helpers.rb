4cbd383fa352399ca53c878bfd5b9e38d8b9ca9c
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-06-12 11:52:41 -0700

    migration error

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,15 @@
 class Helpers
+    
+    def self.current_user(session)
+        @user = User.find_by(id:session[:user_id])    
+    end
+
+    def self.logged_in?(session)
+        # binding.pry
+        if session[:user_id]
+            self.current_user(session).id = session[:user_id]
+        else 
+            false
+        end
+    end
 end
