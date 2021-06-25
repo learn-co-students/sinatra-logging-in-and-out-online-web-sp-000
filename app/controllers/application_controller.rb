f0840bc14d8e167e70f026ed8c3f4b9060e11c90
VandelayUtd <drewhadley.guitar@gmail.com>
2021-06-25 17:26:22 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,13 +12,30 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
 
+    @user = User.find_by(username: params[:username])
+    if @user
+      session[:user_id] = @user.id
+      redirect to '/account'
+    else 
+      erb :error
+    end
+
   end
 
   get '/account' do
-
+    
+    @current_user = User.find_by(id: session[:user_id])
+    
+    if @current_user
+    erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
+    session.clear
+    redirect to '/'
 
   end
 
